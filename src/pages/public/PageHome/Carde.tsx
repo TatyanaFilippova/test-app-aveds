450a534c3debfae7e6796cf82db2edf9b05ab300
Tatyana <tatiana.filippova.98@mail.ru>
2024-09-21 09:10:48 +0300

    сделала карточки

---
+++ src/pages/public/PageHome/Carde.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+import styled  from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 248px;
+    box-shadow: 0px 13px 19px 0px #00000012;
+    padding: 35px 40px;
+    width: 33.33%;
+
+`
+
+interface CardProps{
+    title: string;
+    icon: string;
+    content: string;
+
+}
+
+const IconCard = styled.img`
+    height: 72px;
+    width: 72px;
+    margin-bottom: 20px;
+
+`
+
+const Title = styled.div`
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 20px;
+`
+const Content = styled.div`
+    font-size: 14px;
+    color: #737373;
+`
+const Line = styled.div`
+    width: 50px;
+    border-bottom: 2px solid #E74040;
+    margin-bottom: 20px;
+`
+
+const Card: FC<CardProps> = ({icon, title, content }) =>{
+    return( 
+        <Wrapper>
+            <IconCard src={icon}/>
+            <Title>{title}</Title>
+            <Line/>
+            <Content>{content}</Content>
+            
+
+        </Wrapper>
+    )
+}
+
+export default Card
--- src/pages/public/PageHome/PageHome.tsx
+++ src/pages/public/PageHome/PageHome.tsx
@@ -5,6 +5,11 @@ import styled from "styled-components";
 import {buttonPrimary, layout} from "../../../styles";
 import { buttonOutline } from "../../../styles";
 import { Link } from "react-router-dom";
+import Card from "./Carde";
+import iconHeart from '../../../assets/iconHeart.png'
+import iconStethoscope from '../../../assets/iconStethoscope.png'
+import iconTable from '../../../assets/iconTable.png'
+
 
 const Wrapper = styled.div`
     ${layout}
@@ -19,6 +24,7 @@ const Title = styled.h1`
 const WrapperButton = styled.div`
     display: flex;
     margin-top: 35px;
+    margin-bottom: 96px;
 `
 
 const ButtonLogin = styled.button`
@@ -28,6 +34,12 @@ const ButtonLogin = styled.button`
 const ButtonOutline = styled(Link)`
     ${buttonOutline};
     margin-left: 10px;
+`
+const WrapperCard = styled.div`
+    display: flex;
+    justify-content: space-between;
+    grid-gap: 30px;
+    
 `
 
 const PageHome: FC = () =>{
@@ -40,7 +52,11 @@ const PageHome: FC = () =>{
                     <ButtonLogin>Войти</ButtonLogin>
                     <ButtonOutline to={'/contacts'}>Контакты</ButtonOutline>
                 </WrapperButton>
-                
+                <WrapperCard>
+                    <Card icon={iconHeart} title="Онлайн-прием" content="Рыба текст"/>
+                    <Card icon={iconStethoscope} title="Экстренный Случай" content="Рыба текст"/>
+                    <Card icon={iconTable} title="Лечение рака" content="Рыба текст"/>
+                </WrapperCard>
             </Wrapper>
         </>
     )

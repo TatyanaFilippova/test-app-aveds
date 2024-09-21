45579383bf7320cc6e196bdf4f9ef7d982787586
Tatyana <tatiana.filippova.98@mail.ru>
2024-09-21 09:14:41 +0300

    страница Контакты

--- src/pages/public/PageContacts/PageContacts.tsx
+++ src/pages/public/PageContacts/PageContacts.tsx
@@ -1,13 +1,25 @@
 
 import React, { FC } from "react";
 import Header from "../../../components/Header/Header";
+import styled from "styled-components";
+import { layout } from "../../../styles";
 
+const Wrapper = styled.div`
+    ${layout}
+`
+const Title = styled.div`
+    font-size: 58px;
+    font-weight: bold;
+    margin-top: 184px;
+`
 
 const PageContacts: FC = () =>{
     return(
         <div>
             <Header/>
-            <h1>Контакты</h1>
+            <Wrapper>
+                <Title>Контакты</Title>
+            </Wrapper>
         </div>
     )
 }
